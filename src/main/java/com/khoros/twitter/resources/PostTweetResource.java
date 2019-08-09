1acdc938ef503327944d8840d7d64dfaa15fdb1f
Michelle <michelle.gu@khoros.com>
2019-08-09 15:14:11 -0500

    Fixed error messages to say useful stuff

--- src/main/java/com/khoros/twitter/resources/PostTweetResource.java
+++ src/main/java/com/khoros/twitter/resources/PostTweetResource.java
@@ -2,10 +2,8 @@ package com.khoros.twitter.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import com.khoros.twitter.core.Message;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import twitter4j.*;
+
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -26,25 +24,30 @@ public class PostTweetResource {
         // Post message
         String text = message.getText();
         Status status = null;
+        Message statusMessage = new Message();
         try {
             Twitter twitter = TwitterFactory.getSingleton();
             if (text.length() > 0 || text.length() <= 280) {
                 status = twitter.updateStatus(text);
             } else {
+                statusMessage.setText("Invalid tweet: Tweet must be between 1-280 characters.");
                 return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                        .header("Invalid tweet","Tweet must be between 1-280 characters.")
-                        .type(MediaType.APPLICATION_JSON_TYPE)
+                        .entity(statusMessage)
+                        .type(MediaType.APPLICATION_JSON)
                         .build();
             }
         } catch (TwitterException e) {
+            statusMessage.setText("Twitter exception: " + e.getErrorMessage() + " Tweet must be between 1-280 characters.");
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                    .header("Twitter exception", e.getErrorMessage())
-                    .type(MediaType.APPLICATION_JSON_TYPE)
+                    .entity(statusMessage)
+                    .type(MediaType.APPLICATION_JSON)
                     .build();
         }
 
+        statusMessage.setText("Successfully tweeted: " + status.getText());
         return Response.status(Response.Status.OK)
-                .header("Successfully tweeted", status.getText())
+                .entity(statusMessage)
+                .type(MediaType.APPLICATION_JSON)
                 .build();
     }
 }
