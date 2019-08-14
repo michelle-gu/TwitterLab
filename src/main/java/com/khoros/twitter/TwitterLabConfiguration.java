04de03dbbdae282e5ba50b1f6d16fe7e066af2b7
Michelle <michelle.gu@khoros.com>
2019-08-14 10:30:03 -0500

    Config file getters seters and fields

--- src/main/java/com/khoros/twitter/TwitterLabConfiguration.java
+++ src/main/java/com/khoros/twitter/TwitterLabConfiguration.java
@@ -1,7 +1,61 @@
 package com.khoros.twitter;
 
 import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
 
 public class TwitterLabConfiguration extends Configuration {
+
+    @NotEmpty
+    private String consumerKey;
+
+    @NotEmpty
+    private String consumerSecret;
+
+    @NotEmpty
+    private String accessToken;
+
+    @NotEmpty
+    private String accessTokenSecret;
+
+    @JsonProperty
+    public String getConsumerKey() {
+        return consumerKey;
+    }
+
+    @JsonProperty
+    public void setConsumerKey(String consumerKey) {
+        this.consumerKey = consumerKey;
+    }
+
+    @JsonProperty
+    public String getConsumerSecret() {
+        return consumerSecret;
+    }
+
+    @JsonProperty
+    public void setConsumerSecret(String consumerSecret) {
+        this.consumerSecret = consumerSecret;
+    }
+
+    @JsonProperty
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    @JsonProperty
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    @JsonProperty
+    public String getAccessTokenSecret() {
+        return accessTokenSecret;
+    }
+
+    @JsonProperty
+    public void setAccessTokenSecret(String accessTokenSecret) {
+        this.accessTokenSecret = accessTokenSecret;
+    }
 
 }
