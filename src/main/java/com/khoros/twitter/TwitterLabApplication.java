b3269478798f0b6c9719902dcca8279ebc47bcd2
Michelle <michelle.gu@khoros.com>
2019-08-28 16:10:01 -0500

    add CORS headers to backend

--- src/main/java/com/khoros/twitter/TwitterLabApplication.java
+++ src/main/java/com/khoros/twitter/TwitterLabApplication.java
@@ -4,8 +4,12 @@ import com.khoros.twitter.resources.TwitterLabResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TwitterLabApplication extends Application<TwitterLabConfiguration> {
 
@@ -28,6 +32,16 @@ public class TwitterLabApplication extends Application<TwitterLabConfiguration> 
     @Override
     public void run(TwitterLabConfiguration configuration,
                     Environment environment) {
+        // Enable CORS headers
+        final FilterRegistration.Dynamic cors =
+                environment.servlets().addFilter("CORS", CrossOriginFilter.class);
+        // Configure CORS parameters
+        cors.setInitParameter("allowedOrigins", "*");
+        cors.setInitParameter("allowedHeaders", "X-Requested-With,Content-Type,Accept,Origin");
+        cors.setInitParameter("allowedMethods", "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        // Add URL mapping
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+
         LOGGER.info("Running TwitterLab application.");
         LOGGER.debug("Registering twitter lab resource with twitter instance.");
         TwitterLabComponent component = DaggerTwitterLabComponent.builder()
